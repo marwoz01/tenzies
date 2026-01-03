46273516cc8365d52c489a892f968851057c3988
Marcel <marwozniak01@gmail.com>
2026-01-03 12:50:01 +0100

    10 random numbers

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,6 +1,12 @@
 import Die from "./Die";
 
 export default function App() {
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
+  }
+
+  console.log(generateAllNewDice());
+
   return (
     <main>
       <div className="dice-container">
