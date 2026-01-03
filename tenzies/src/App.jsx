36da79eee5e8d0e766eb7ec206dc08a2bb20b3ab
Marcel <marwozniak01@gmail.com>
2026-01-03 13:10:50 +0100

    mapping array to die components

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -1,26 +1,18 @@
+import { useState } from "react";
 import Die from "./Die";
 
 export default function App() {
+  const [dice, setDice] = useState(generateAllNewDice());
+
   function generateAllNewDice() {
     return new Array(10).fill(0).map(() => Math.ceil(Math.random() * 6));
   }
 
-  console.log(generateAllNewDice());
+  const diceElements = dice.map((num) => <Die value={num} />);
 
   return (
     <main>
-      <div className="dice-container">
-        <Die value={1} />
-        <Die value={2} />
-        <Die value={3} />
-        <Die value={4} />
-        <Die value={5} />
-        <Die value={6} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-        <Die value={1} />
-      </div>
+      <div className="dice-container">{diceElements}</div>
     </main>
   );
 }
